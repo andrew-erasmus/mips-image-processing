d2e986bfe7534b2c3d865ce705fa8946c58a3e9f
andrew-erasmus <andrew.r.eras@gmail.com>
2023-09-27 10:08:23 +0200

    Fix structure of greyscale.asm and add logic to change p3 to p3

--- greyscale.asm
+++ greyscale.asm
@@ -21,16 +21,27 @@ main:
     syscall
     move $s0, $v0 # save the file name
 
-
+    #open file for writing
+    li $v0, 13
+    la $a0, output_file
+    li $a1, 1
+    syscall
+    move $s1, $v0
+ 
     li $t0, 0 #the number to be incremented - will be set to zero when a new number is read in
     li $t1, 0 #the position counter for string to int
 
     li $t3, 0 # current pos for length loop
-    li $t4, 0 # counter for the length of image content string
+    li $t4, 0 # counter for the length of output_string
 
     li $t5, 0 # the digit counter of the temp string
     li $t6, 0 # the position counter for the int to string
     li $t7, 0 #counter to skip first 3 lines - will increment if equals "\n"
+
+    li $t8, 0 #sum of the averages before increase
+    li $t9, 0 #sum of averages after increase
+
+    li $s6, 0 #counter for output purposes
 
 
 read_file:
@@ -52,12 +63,22 @@ read_file:
 skip_three_lines:
     beq $t7, 3, ascii_to_int
     lb $t2, image_content($t1)
-   
+
+    beq $t2, 51, change_p3_to_p2 #checks if it is equal to '3'
+
     sb $t2, output_string($t6) # will add to output string but skip over for brightness processing
     addi $t6, $t6, 1
     
     beq $t2, 10, incr_skip_counter
     
+    addi $t1, $t1, 1
+    j skip_three_lines
+
+change_p3_to_p2:
+    li $t2, 50
+    sb $t2, output_string($t6) # will add to output string but skip over for brightness processing
+    addi $t6, $t6, 1
+
     addi $t1, $t1, 1
     j skip_three_lines
 
@@ -69,12 +90,11 @@ incr_skip_counter: #skip counter is the one that counts the first 3 lines when e
     j skip_three_lines
 
 
-
 ascii_to_int:
     
     beq $t2, 10, check_end # Checks if it is a CR to then check if it is a LF
     lb $t2, image_content($t1)
-    beq $t2, 10, greyscale_calc # if the line is finished, continue process of adding to the integer 
+    beq $t2, 10, incr_by_ten # if the line is finished, continue process of adding to the integer 
 
     #operations to convert to int
     sub $t2, $t2, 48
@@ -85,14 +105,25 @@ ascii_to_int:
     j ascii_to_int
 
  check_end:
-     addi $t1, $t1, 1
-     lb $t2, image_content($t1)
-     beq $t2, 13, write_to_file
-     addi $t1, $t1, -1
-     j ascii_to_int
+    addi $t1, $t1, 1
+    lb $t2, image_content($t1)
+    beq $t2, 13, count_output_string
+    addi $t1, $t1, -1
+    j ascii_to_int
     
-greyscale_calc:
-        
+incr_by_ten:
+    add $t8, $t8, $t0 # add $t0 to the sum of the pixels
+    li $s7, 255
+    addi $t0, $t0, 10 #increase by 10
+    bge $t0, $s7, skip_add_ten #skip increment by 10 if its at the 255 limit
+    add $t9, $t9, $t0 # add $t0 to the sum of the pixels after the increase
+    j int_to_ascii
+
+skip_add_ten:
+    li $t0, 255 #if will go over then set it to 255
+    add $t9, $t9, $t0 # add $t0 to the sum of the pixels after the increase
+    j int_to_ascii
+    
 
 int_to_ascii:
     #divide by 10 to get the unit
@@ -133,9 +164,20 @@ end_int_to_ascii:
     addi $t1, $t1, 1
     j ascii_to_int
 
+count_output_string:
+    lb $t2, output_string($s6)
+    beqz $t2, finish_off
+
+    addi $s6, $s6, 1
+    addi $t4, $t4, 1 
+    j count_output_string
+
+finish_off:
+    li $t2, 0 #counter for the position of the byte in the output
+    j write_to_file
 
 write_to_file:
-
+    
     li $v0, 13
     la $a0, output_file
     li $a1, 1
@@ -154,12 +196,8 @@ write_to_file:
     move $a0, $s7
     syscall
 
-
 #exit the program
 exit:
 
     li $v0, 10
     syscall
-
-
-# ! Find out why file ends in Nulls
